04de25d96e3bf35fe0a02fd7958225d278b82b08
rrc_F2024FA_awilliams <awilliams3@academic.rrc.ca>
2025-03-17 14:05:55 -0500

    Ticket 2. details

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import moderationRoutes from "./api/v1/routes/moderationRoutes";
 import  setupSwagger  from '../config/swagger';
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
 
 const app: Express = express();
 app.use(express.json());
